312b71926d37f1f2c71fb74f56b1f9f8eb0edb33
Nikita Zyuzin <nzyuzin93@gmail.com>
2018-10-09 21:45:10 +0200

    Fix primitive +

--- lang.ml
+++ lang.ml
@@ -1,5 +1,7 @@
 exception ExecutionError of string
 
+let error str = raise (ExecutionError ("execution error: " ^ str))
+
 type ident = string
 type var_type = ident
 type const_type = string
@@ -15,6 +17,7 @@ type tml_expr =
   | EVar of var_type
   | EVal of tml_val
   | EAppl of tml_expr * tml_expr
+  | EPrimAppl of ident * (tml_expr list)
   | EPair of tml_expr * tml_expr
 and tml_val =
   | VUnit
@@ -46,6 +49,7 @@ let rec subst_expr (expr: tml_expr) (var: var_type) (sub: tml_val): tml_expr =
   | EVar var' as same -> if var' = var then EVal sub else same
   | EVal v -> EVal (subst_val v var sub)
   | EAppl (s, t) -> EAppl (subst_expr s var sub, subst_expr t var sub)
+  | EPrimAppl (f, l) -> EPrimAppl (f, List.map (fun e -> subst_expr e var sub) l)
   | EPair (f, s) -> EPair (subst_expr f var sub, subst_expr s var sub)
 and subst_val (value: tml_val) (var: var_type) (sub: tml_val): tml_val =
   match value with
@@ -63,26 +67,41 @@ let rec eval (expr: tml_expr) (env: environment_type): tml_val =
     end
   | EVal v -> v
   | EAppl (s, t) -> apply s t env
+  | EPrimAppl (f, l) -> primitive_apply f l env
   | EPair (f, s) -> VPair (eval f env, eval s env)
 and apply (f: tml_expr) (arg: tml_expr) (env: environment_type): tml_val =
   match eval f env with
   | VAbs (v, e) -> let argval = eval arg env in
                    eval (subst_expr e v argval) env
-  | _ -> raise (ExecutionError "Bad application")
-
-let primitive_plus (x: tml_expr) (y: tml_expr): tml_expr =
-  match x, y with
-  | (EVal (VInt x'), EVal (VInt y')) -> EVal (VInt (x' + y'))
-  | _ -> raise (ExecutionError "Unexpected input to \"+\"")
+  | _ -> error "bad application"
+and primitive_apply (f: ident) (l: tml_expr list) (env: environment_type): tml_val =
+  let evaled_l = List.map (fun e -> eval e env) l in
+  let check_arguments sz f =
+    let actual_size = List.length l in
+    if actual_size != sz then error ("unexpected number of arguments to " ^ f
+                                     ^ ": " ^ string_of_int actual_size
+                                     ^ "instead of " ^ string_of_int sz) in
+  match f with
+  | "+" as p -> let _ = check_arguments 2 p in
+                begin
+                  match (List.nth evaled_l 0, List.nth evaled_l 1) with
+                  | (VInt x'), (VInt y') -> VInt (x' + y')
+                  | _ -> error "unexpected input to \"+\""
+                end
+  | _ -> error "unkown primitive function"
 
 let initial_env = ("id", (VAbs ("x", EVar "x")))
-                      :: (("+"), (VAbs ("x", EVal (VAbs ("y",
-                                                         primitive_plus
-                                                           (EVar "x")
-                                                           (EVar "y"))))))
-                      :: []
+                  :: ("+", (VAbs ("x", EVal (VAbs ("y", EPrimAppl
+                                                          ("+",
+                                                           [(EVar "x");
+                                                            (EVar "y")]))))))
+                  :: []
 
 let () =
-  let expr = EAppl (EVar "id", (EAppl (EVar "+", EVal (VInt 9)))) in
+  let expr = EAppl (EVar "id", (EAppl (EAppl (EVar "+", EVal (VInt 9)),
+                                       EVal (VInt 10)))) in
+  let expr2 = EAppl (EVar "+", EVal (VInt 9)) in
   let ev = eval expr initial_env in
-  print_endline (string_of_val ev)
+  let ev2 = eval expr2 initial_env in
+  let _ = print_endline (string_of_val ev) in
+  print_endline (string_of_val ev2)
